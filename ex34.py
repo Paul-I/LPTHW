8e3b44351fe7260b8326f65a80845b1228e5a067
Paul Ijagbemi <paulijagbemi@seoulchristian.org>
2016-01-12 16:20:00 +0900

    ex34.py

---
+++ ex34.py
@@ -0,0 +1,25 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+1. The animal at (1) is at 0 in python.
+   The 0 zero position in python is 1 for the animal.
+
+2. The third (3rd) animal is at the 2 in python.
+    The number 2 in python is the third (3rd) animal.
+
+3. The first (1st) animal is at 0 in python number.
+   The number 0 in python is the first (1st) animal.
+
+4. The animal at 3 (third) is at 2 in python.
+   The number 2 in python is the 3rd animal.
+
+5. The fifth (5th) animal is at 4 in python.
+   The number 4 in python is the fifth (5th) animal.
+
+6. The animal at 2 is at 1 in python.
+   The number 1 in python is the 2 (2rd) animal.
+
+7. The sixth (6th) animal is the number 5 in python.
+   The number 5 in python is the sixth (6th) animal.
+
+8. The animal at 4 is at 3 in python.
+   The number 3 in python is the 4 animal.
